1510dbc699b832321ed3009d2317d2ccc8027e67
Leonel Eugenio Gomez Gomez <inf.leonel@gmail.com>
2024-08-13 18:02:27 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -32,9 +32,9 @@ app.use(cors());
 app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(favicon(path.join('../public/', 'favicon.ico')));
+app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
 // Host the public folder
-app.use('/', express.static('../public/'));
+app.use('/', express.static(app.get('public')));
 
 // Set up Plugins and providers
 app.configure(express.rest());
